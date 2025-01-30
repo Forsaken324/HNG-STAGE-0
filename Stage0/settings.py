1b9d4e07a2d7aaf05d3a550872e12ccbe54759db
David <davidnduonofit47@gmail.com>
2025-01-30 22:59:29 +0100

    Resolved conflicts

--- Stage0/settings.py
+++ Stage0/settings.py
@@ -38,20 +38,28 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework',
+    'corsheaders',
 
-    'app.apps.AppConfig'
+    'rest_framework',
+    
+    'app.apps.AppConfig',
+
+    
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'Stage0.urls'
 
